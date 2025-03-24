2d225d8c96071db52b546b8b5158e46a69bc6be8
AR_Leal <anarosalm@outlook.com>
2025-03-24 16:40:43 -0600

    carrusel funcionando correctamente

---
+++ js/carrusel.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true
+});
+
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[5000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
